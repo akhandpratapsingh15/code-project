33fa57d33122ee78edee3b1033e0b64e0c710f6d
akhandpratapsingh15 <akhand7897239198@gmail.com>
2025-08-03 09:51:16 +0530

    Create calculator.c
    code about calculator

---
+++ calculator.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator); // Notice the space before %c to consume any leftover newline
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            printf("Result: %.2lf\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %.2lf\n", num1 - num2);
+            break;
